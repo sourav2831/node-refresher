f5b68da3c2749bd1d88f47767512d5af8fbc51ab
sourav2831 <sourav2831@gmail.com>
2024-01-23 13:22:49 +0530

    created file using fs module

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   console.log(req);
@@ -10,6 +11,20 @@ const server = http.createServer((req, res) => {
       "<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>Submit</button></form></body>"
     );
     res.write("</html>");
+    return res.end();
+  }
+  if (req.url === "/message") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
     return res.end();
   }
   res.setHeader("Content-Type", "text/html");
