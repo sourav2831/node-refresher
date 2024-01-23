08d8344dd3e57ec2a41b05767f27e84455b8c87f
sourav2831 <sourav2831@gmail.com>
2024-01-23 12:54:15 +0530

    sent response to server

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const http = require("http");
 
-const server = http.createServer((res, req) => {
+const server = http.createServer((req, res) => {
   console.log(req);
-  process.exit();
+  //process.exit();
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<body>Nodejs Server Response</body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
