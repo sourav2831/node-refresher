9329200adc5b855751a41e1bf26a067f1cb3b249
sourav2831 <sourav2831@gmail.com>
2024-01-23 13:16:59 +0530

    added conditional routing

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   console.log(req);
   //process.exit();
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>Submit</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<body>Nodejs Server Response</body>");
